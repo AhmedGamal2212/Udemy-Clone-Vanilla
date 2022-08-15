36e3c5ef871cbadb019e43f29770ad48e738040c
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-08-15 14:54:35 +0200

    feat: Optimize fetch method, Add loadCourses function

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let files = [];
+let files;
 let currSection = "";
-let hostURL = 'http://localhost:3000/';
+const hostURL = 'https://api.jsonbin.io/v3/b/62fa3b83a1610e6386ff2396/';
 
 const loadContent = async () => {
-    files = [];
 
-    let courses_sections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"];
+    let coursesSections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"];
     
-    for(let section of courses_sections){
-        let response = await fetch(hostURL + section);
-        files.push(await response.json());
-    }
+    let response = await fetch(hostURL);
+    files = await response.json()
 
     const container = document.querySelector('.courses-cards');
-
     container.innerHTML = '';
 
-    for(let file of files){
-        file.courses.forEach(element => {
+    loadCourses(coursesSections, container);
+} 
+
+function loadCourses(coursesSection, container){
+    container.innerHTML = '';
+
+    for(let file of coursesSection){
+        files['record'][file].courses.forEach(element => {
             let inst = '';
 
             element.instructors.forEach(instructor => {
@@ -57,63 +59,24 @@ const loadContent = async () => {
             container.innerHTML += child;
         });
     }
-} 
+}
+
+
+// TODO: add the title and description when changing the section of the courses
 
 const changeSection = async () => {
-    let input_form = [...document.querySelector('.courses-radio').children];
+    let inputForm = [...document.querySelector('.courses-radio').children];
+    let coursesSections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"]
+    const container = document.querySelector('.courses-cards');
 
-    for(const lebel of input_form){
+    for(const lebel of inputForm){
         for(const child of lebel.children){
             if(child.tagName == "INPUT" && child.checked){
                 if(currSection.length == 0 || currSection != child.value){
                     if(child.value == 'all'){
-                        loadContent();
+                        loadCourses(coursesSections, container);
                     }else{
-                        const response = await fetch(hostURL + child.value);
-                        const file = await response.json();
-                        const container = document.querySelector('.courses-cards');
-
-                        container.innerHTML = '';
-
-                        file.courses.forEach(element => {
-                            let inst = '';
-                
-                            element.instructors.forEach(instructor => {
-                                inst += instructor.name + ', ';
-                            });
-                            
-                            inst = inst.slice(0, -1);
-                
-                            const child = `
-                            <article class="course-card">
-                            <!-- course image -->
-                            <figure>
-                                <img src="${element.image}">
-                            </figure>
-                            <!-- course info -->
-                            <section class="card-info">
-                                <!-- course title -->
-                                <h2>${element.title}</h2>
-                
-                                <!-- author -->
-                                <p>${inst}</p>
-                
-                                <!-- rating -->
-                                <section class="rating">
-                                    <p>${element.rating.toPrecision(2)}</p>
-                                    <i class="fa-solid fa-star stars"></i>
-                                    <i class="fa-solid fa-star stars"></i>
-                                    <i class="fa-solid fa-star stars"></i>
-                                    <i class="fa-solid fa-star stars"></i>
-                                    <i class="fa-regular fa-star-half-stroke stars"></i>
-                                </section>
-                                <!-- price -->
-                                <p class="price">$${element.price}</p>
-                            </section>
-                        </article>
-                            `;
-                            container.innerHTML += child;
-                        });
+                        loadCourses([child.value], container);
                     }
                     currSection = child.value;
                 }
