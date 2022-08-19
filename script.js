6ca854bdeffd75a571741dcf1601b467fa65a866
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-08-19 18:52:03 +0200

    feat: Refactor the whole script.js file, Redesign courses section using Bootstrap

--- script.js
+++ script.js
@@ -1,69 +1,154 @@
-let files;
-let currSection = "";
 const hostURL = 'https://api.jsonbin.io/v3/b/62fa3b83a1610e6386ff2396/';
+const coursesSections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"];
+let sectionObject = {}, currentSection = '', lastSearchedWord = '', coursesPerRow = 1;
 
-const loadContent = async () => {
-
-    let coursesSections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"];
-    
-    let response = await fetch(hostURL);
-    files = await response.json()
-
-    const container = document.querySelector('.courses-cards');
-    container.innerHTML = '';
-
-    loadCourses(coursesSections, container);
-} 
-
-function loadCourses(coursesSections, container){
-    container.innerHTML = '';
-
-    for(let file of coursesSections){
-        files['record'][file].courses.forEach(element => {
-            let inst = '';
-
-            element.instructors.forEach(instructor => {
-                inst += instructor.name + ', ';
-            });
-            
-            inst = inst.slice(0, -1);
-
-            const child = `
-            <article class="course-card ${file}-course">
-            <!-- course image -->
-            <figure>
-                <img src="${element.image}">
-            </figure>
-            <!-- course info -->
-            <section class="card-info">
-                <!-- course title -->
-                <h2>${element.title}</h2>
-
-                <!-- author -->
-                <p>${inst}</p>
-
-                <!-- rating -->
-                <section class="rating">
-                    <p>${element.rating.toPrecision(2)}</p>
-                    <i class="fa-solid fa-star stars"></i>
-                    <i class="fa-solid fa-star stars"></i>
-                    <i class="fa-solid fa-star stars"></i>
-                    <i class="fa-solid fa-star stars"></i>
-                    <i class="fa-regular fa-star-half-stroke stars"></i>
-                </section>
-                <!-- price -->
-                <p class="price">$${element.price}</p>
-            </section>
-        </article>
-            `;
-            container.innerHTML += child;
-        });
-    }
-
-    showCards('python');
+const loadCourses = async () => {
+    fetch(hostURL).then(response => response.json())
+    .then(jsonFile => {
+        for(const section of coursesSections){
+            sectionObject[section] = jsonFile['record'][section];
+        }
+        
+        changeSection(false);
+    });
 }
 
-function showCards(category){
+function changeSection(resize){
+    const inputForm = [...document.querySelector('.courses-radio').children];
+    const searchBarValue = document.querySelector('.bar').value.toLowerCase();
+
+    let section = '';
+
+    let coursesToLoad = [];
+    for(const label of inputForm){
+        for(const child of label.children){
+            if(child.tagName == "INPUT" && child.checked){
+                coursesToLoad = sectionObject[child.value]['courses'].filter((course) => {
+                    return course['title'].toLowerCase().includes(searchBarValue);
+                });
+
+                section = child.value;
+            }
+        }
+    }
+
+    if(lastSearchedWord == searchBarValue && currentSection == section && !resize)
+        return;
+
+
+    changeSectionHeader(section);
+
+    currentSection = section;
+    lastSearchedWord = searchBarValue;
+
+
+    const carousel = document.querySelector('#coursesCarousel .carousel-inner');
+    carousel.innerHTML = '';
+
+    const coursesRows = createRows(coursesToLoad);
+
+    carousel.innerHTML += coursesRows;
+}
+
+function createRows(coursesToLoad){
+    if(!coursesToLoad.length)
+        return ``;
+
+    const numberOfRows = Math.ceil(coursesToLoad.length / coursesPerRow);
+
+    let coursesRows = ``;
+    let currentCourse = 0;
+
+    for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+        let row = ``;
+        for(let courseIndex = currentCourse; courseIndex < Math.min(coursesToLoad.length, currentCourse + coursesPerRow); courseIndex++){
+            row += createCourse(coursesToLoad[courseIndex]);
+        }
+        currentCourse += coursesPerRow;
+        
+        coursesRows += `
+        <div class="carousel-item`;
+        if(!rowIndex){
+            coursesRows += ` active `;
+        }
+        coursesRows += `">`;
+        coursesRows += `
+            <div class="row">
+        `
+        coursesRows += row;
+        coursesRows += `
+            </div>
+        </div>`;
+    }
+
+    return coursesRows;
+}
+
+function createCourse(course){
+
+    let instructors = ``;
+
+    course['instructors'].forEach((instructor) => {
+        instructors += ' ' + instructor['name'] + ',';
+    });
+
+    instructors = instructors.slice(0, -1);
+
+    return `
+    <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch">
+        <article class="card h-100 d-inline-block shadow">
+            <figure class="img-wrapper">
+                <img src="${course.image}" alt="" class="d-block w-100 h-100">
+            </figure>
+            <section class="card-body">
+                <h3 class="course-title">
+                    ${course.title}
+                </h3>
+                <p class="course-instructors">
+                    ${instructors}
+                </p>
+                <section class="course-rating-container">
+                    <p class="course-rating">${course.rating.toPrecision(2)}</p>
+                    ${getRating(course.rating.toPrecision(2))}
+
+                </section>
+                <p class="course-price">
+                    $${course.price}
+                </p>
+            </section>
+        </article>
+    </div>
+    `
+}
+
+
+function getRating(rating){
+
+    let courseRating = ``, star = 0;
+    for(star = 0; star < Math.floor(rating); star++){
+        courseRating += `
+            <i class="fa-solid fa-star stars"></i>
+        `;
+    }
+    
+    if(!Number.isInteger(rating)){
+        star++;
+        courseRating += `
+            <i class="stars fa-regular fa-star-half-stroke"></i>
+        `;
+    }
+
+    while(star < rating){
+        star++;
+        courseRating += `
+        <i class="fa-light fa-star"></i>
+        `
+    }
+
+    return courseRating;
+}
+
+function changeSectionHeader(section){
 
     const buttonValue = {
         'python':'Python',
@@ -75,63 +160,44 @@ function showCards(category){
         'web-dev': 'Web Development'
     }
 
-    const title = document.querySelector('.courses-section-header');
-    title.innerHTML = files['record'][category]['header'];
+    const header = document.querySelector('.courses-section-header');
+    header.innerHTML = sectionObject[section].header;
+
 
     const description = document.querySelector('.courses-section-description');
-    description.innerHTML = files['record'][category]['description'];
+    description.innerHTML = sectionObject[section].description;
 
     const exploreButton = document.querySelector('.explore');
-    exploreButton.innerHTML = 'Explore ' + buttonValue[category];
-
-    let cards = document.querySelectorAll('.course-card');
-
-    cards.forEach(card => {
-        card.style.display = 'none';
-    });
-
-    cards = document.querySelectorAll(`.${category}-course`);
-    
-    cards.forEach(card => {
-        card.style.display = 'block';
-    });
-}
-
-const changeSection = async () => {
-    const inputForm = [...document.querySelector('.courses-radio').children];
-    const coursesSections = ["data-science", "drawing", "aws", "excel", "js", "python", "web-dev"]
-    const container = document.querySelector('.courses-cards');
-
-    for(const lebel of inputForm){
-        for(const child of lebel.children){
-            if(child.tagName == "INPUT" && child.checked){
-                if(currSection.length == 0 || currSection != child.value){
-                    showCards(child.value);
-                    currSection = child.value;
-                }
-            }
-        }        
-    }
-}
-
-function searchBar(){
-    const bar = document.querySelector('.bar');
-    const val = bar.value.toLowerCase();
-    const container = document.querySelector('.courses-cards');
-
-    for(let card of container.children){
-        card.style.display = 'block';
-    }
-
-    if(val.length){
-        for(let card of container.children){
-            const currValue = card.querySelector('.card-info').getElementsByTagName('h2')[0].innerHTML.toLowerCase();
-            if(currValue.includes(val))
-                continue;
-            card.style.display = 'none';
-        }
-    }
+    exploreButton.innerHTML = 'Explore ' + buttonValue[section];
 
 }
 
-window.addEventListener('DOMContentLoaded', () => loadContent());
+function checkMediaQuery(){
+    let result = 1;
+
+    if(window.matchMedia('(min-width: 1200px)').matches){
+        result = 4;
+    }else if(window.matchMedia('(min-width: 991px)').matches){
+        result = 3;
+    }else if(window.matchMedia('(min-width: 768px)').matches){
+        result = 2;
+    } 
+
+    return result;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    coursesPerRow = checkMediaQuery();
+    loadCourses();
+});
+
+window.addEventListener('resize', () => {
+    const newNumberPerRow = checkMediaQuery();
+    console.log(newNumberPerRow)
+    if(newNumberPerRow != coursesPerRow){
+        coursesPerRow = newNumberPerRow;
+        changeSection(true);
+        console.log('changed')
+    }
+});
+
